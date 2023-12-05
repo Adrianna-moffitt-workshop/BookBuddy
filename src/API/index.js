d65abcf3495deeeb04c481fd0edb6fa4737e7bb2
Adrianna <adriannamoffitt06@gmail.com>
2023-12-05 10:41:45 -0600

    API

---
+++ src/API/index.js
@@ -0,0 +1,123 @@
+const baseUrl = "http://fsa-book-buddy-b6e748d1380d.herokuapp.com/api";
+
+export async function getAllBooks() {
+  try {
+    const response = await fetch(`${baseUrl}/books`, {
+      headers: { " Content-Type": "application/json" },
+    });
+    const booksArray = await response.json();
+    return booksArray.books;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function register(user) {
+  try {
+    const response = await fetch(`${baseUrl}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const json = await response.json();
+    return json;
+  } catch (errpr) {
+    console.log(error);
+  }
+}
+
+export async function apiLogIn(user) {
+  try {
+    const response = await fetch(`${baseUrl}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getUserInfo(token) {
+  try {
+    const response = await fetch(`${baseUrl}/users/me`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function rentBookApi(userKey, bookId) {
+  try {
+    const response = await fetch(`${baseUrl}/books/${bookId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userKey}`,
+      },
+      body: JSON.stringify({ available: false }),
+    });
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function reservationListApi(userId) {
+  try {
+    const response = await fetch(`${baseUrl}/reservations`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userId}`,
+      },
+    });
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function returnBookApi(userId, resId) {
+  try {
+    const response = await fetch(`${baseUrl}/reservations`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userId}`,
+      },
+    });
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function returnBookApi(userId, resId) {
+  try {
+    const response = await fetch(`${baseUrl}/reservation/${resId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userId}`,
+      },
+    });
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
